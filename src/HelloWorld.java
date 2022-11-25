50c888b9b4dbaa9965a72998b919a6c5a12891aa
Dave <dund1983@proton.me>
2022-11-25 12:14:23 +0000

    added more code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main (String[] args) {
+        new Secret secret = new Secret();
+        secret.Password = "newpassword"
+        System.out.println(secret.getPassword);
+        System.out.println(secret.User);
         System.out.println("Hello World");
     }
 }
